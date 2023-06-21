afe6c2d10ccfa639c73d0b6c788e7e94fed2c645
nvyktur <okezkid3@gmail.com>
2023-06-20 18:57:25 -0700

    Hope this works

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
+int main(void)
+{
+        _putchar('_');
+        _putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
@@ -18,4 +18,5 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
+
